41b9f0f02f2d824c05344a87912cebcab5764146
EduQnl <eduardoquinalha@gmail.com>
2021-01-11 22:31:08 -0300

    POST /orders

--- backend/src/main/java/com/sds/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/sds/dsdeliver/controllers/OrderController.java
@@ -1,17 +1,19 @@
 package com.sds.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sds.dsdeliver.dto.OrderDTO;
-import com.sds.dsdeliver.dto.ProductDTO;
 import com.sds.dsdeliver.services.OrderService;
-import com.sds.dsdeliver.services.ProductService;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -26,4 +28,11 @@ public class OrderController {
 		return ResponseEntity.ok().body(list);		
 	}
 	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/sds/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/sds/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.sds.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,12 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sds.dsdeliver.dto.OrderDTO;
+import com.sds.dsdeliver.dto.ProductDTO;
 import com.sds.dsdeliver.entities.Order;
+import com.sds.dsdeliver.entities.OrderStatus;
+import com.sds.dsdeliver.entities.Product;
 import com.sds.dsdeliver.repositories.OrderRepository;
-
-
-
-
+import com.sds.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -21,10 +22,13 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	// Sinaliza que trata-se de conexão ao banco. O readOnly evita fazer o locking de escrita.
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findOrdersWithProducts(){
-		List<Order> list = repository.findAll();
+		List<Order> list = repository.findOrdersWithProducts();
 		
 		// Precisamos retornar uma lista de ProductDTO (estamos transportando para a camada externa)
 		// Sendo assim, converte-se para stream para poder usar map que instancia cada p do List
@@ -32,5 +36,17 @@ public class OrderService {
 		// novamente para List usando collectors.toList()
 		
 		return list.stream().map(o -> new OrderDTO(o)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
